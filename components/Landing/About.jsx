9432030ce53cb6f6f1af7d8c7e5cb00d454d584d
roopau80 <roopauppar80@gmail.com>
2025-01-17 20:45:19 -0600

    first commit

--- components/Landing/About.jsx
+++ components/Landing/About.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import resumeUrl from '../../Resume/roopauppar-cv.pdf'
+import resumeUrl from '../../Resume/roopauppar-Resume.pdf'
 import aboutwomen from '../../assets/images/aboutimg2.png';
 // import react_pdf from 'react_pdf';
 
@@ -15,26 +15,26 @@ const About = () => {
     return (
         <>
             <section id='about' className='py-10 text-white'>
-                <div className='text-center mt-8'>
-                    <h3 className=' text-4xl font-semibold'>
+                <div className='mt-8 text-center'>
+                    <h3 className='text-4xl font-semibold '>
                         <span className="relative before:content-[''] before:absolute before:block before:w-full before:h-[2px] 
               before:bottom-0 before:left-0 before:bg-white
               before:hover:scale-x-100 before:scale-x-0 before:origin-top-left
               before:transition before:ease-in-out before:duration-300">
                             About <span className='text-cyan-600'>Me</span></span>
                     </h3>
-                    <p className=' text-gray-400 my-3 text-lg'>My introduction</p>
-                    <div className='flex md:flex-row flex-col-reverse items-center md:gap-6 gap-12 px-10 max-w-6xl mx-auto'>
+                    <p className='my-3 text-lg text-gray-400 '>My introduction</p>
+                    <div className='flex flex-col-reverse items-center max-w-6xl gap-12 px-10 mx-auto md:flex-row md:gap-6'>
                         <div className='p-2'>
-                            <div className='text-gray-300 my-3'>
-                                <p className='text-justify leading-7 w-11-12 mx-auto'>My name is Roopa, and I am an enthusiastic, self-motivated, and reliable individual who takes her responsibilities seriously. I pride myself on being a hardworking person who is dedicated to achieving goals and consistently meeting expectations. I thrive in a team environment, contributing positively and valuing collaboration, but I'm equally comfortable using my own initiative to tackle challenges. I am highly adaptable and can quickly adjust to different situations, finding innovative solutions when needed. One of my strengths is my ability to perform effectively under pressure, ensuring that I not only meet but exceed strict deadlines. This combination of qualities allows me to excel in various work settings and make a positive impact on any team or project.</p>
-                                <div className='flex mt-10 items-center gap-7 text-center'>
+                            <div className='my-3 text-gray-300'>
+                                <p className='mx-auto leading-7 text-justify w-11-12'>My name is Roopa, and I am an enthusiastic, self-motivated, and reliable individual who takes her responsibilities seriously. I pride myself on being a hardworking person who is dedicated to achieving goals and consistently meeting expectations. I thrive in a team environment, contributing positively and valuing collaboration, but I'm equally comfortable using my own initiative to tackle challenges. I am highly adaptable and can quickly adjust to different situations, finding innovative solutions when needed. One of my strengths is my ability to perform effectively under pressure, ensuring that I not only meet but exceed strict deadlines. This combination of qualities allows me to excel in various work settings and make a positive impact on any team or project.</p>
+                                <div className='flex items-center mt-10 text-center gap-7'>
                                     {
                                         info.map((content) => (
                                             <div className="" key={content.text}>
-                                                <h3 className='md:text-4xl text-2xl font-semibold text-white'>
+                                                <h3 className='text-2xl font-semibold text-white md:text-4xl'>
                                                     {content.count}<span className='text-cyan-600'>+</span>{""}</h3>
-                                                <span className='md:text-base text-xs'>{content.text}</span>
+                                                <span className='text-xs md:text-base'>{content.text}</span>
 
                                             </div>
                                         ))}
@@ -49,9 +49,9 @@ const About = () => {
 
                             </div>
                         </div>
-                        <div className='flex-1 md:mt-0 mb-12 flex justify-center items-center'>
-                            <div className='lg:w-80 h-full relative sm:w-10/12 w-11/12 max-w-sm  aboutImg'>
-                                <img src={aboutwomen} alt='' className='w-full lg:h-50 object-cover bg-slate-200 rounded-xl' />
+                        <div className='flex items-center justify-center flex-1 mb-12 md:mt-0'>
+                            <div className='relative w-11/12 h-full max-w-sm lg:w-80 sm:w-10/12 aboutImg'>
+                                <img src={aboutwomen} alt='' className='object-cover w-full lg:h-50 bg-slate-200 rounded-xl' />
 
 
                             </div>
--- components/Landing/Home.jsx
+++ components/Landing/Home.jsx
@@ -12,25 +12,25 @@ const Home = () => {
     },
     {
       icons: 'logo-github',
-      live_link: "https://github.com/Roopauppar",
+      live_link: "https://github.com/roopau80",
     }
 
   ]
 
   return (
     <>
-      <section id='home' className='min-h-screen flex py-10 md:flex-row flex-col items-center '>
+      <section id='home' className='flex flex-col items-center min-h-screen py-10 md:flex-row '>
 
-        {/* <div className='flex-1 flex items-center justify-center h-full mt-16 space-x-10  realtive  rounded-lg shadow-2xl  overflow-hidden '>
-    <img src={bgimg2} alt='' className='absolute inset-0 h-full  object-cover'></img>
-      <img src={portfoliowomen} alt='' className='md:w-3/4 h-full object-cover mix-blend-darken inset-0 w-full'></img>
+        {/* <div className='flex items-center justify-center flex-1 h-full mt-16 space-x-10 overflow-hidden rounded-lg shadow-2xl realtive '>
+    <img src={bgimg2} alt='' className='absolute inset-0 object-cover h-full'></img>
+      <img src={portfoliowomen} alt='' className='inset-0 object-cover w-full h-full md:w-3/4 mix-blend-darken'></img>
     </div> */}
         {/* <div className='wrapper'> */}
-        <div className='mt-16 flex-1 flex  justify-center space-x-10  '>
-          <div className=' relative w-80 h-80 max-w-full rounded-lg shadow-2xl overflow-hidden '>
+        <div className='flex justify-center flex-1 mt-16 space-x-10 '>
+          <div className='relative max-w-full overflow-hidden rounded-lg shadow-2xl  w-80 h-80'>
             {/* <div className='bubble x8'></div> */}
-            <img src={bgimg2} className='absolute inset-0 w-full h-full object-cover' />
-            <img src={portfoliowomen} className=' absolute inset-0 w-full h-full object-cover' />
+            <img src={bgimg2} className='absolute inset-0 object-cover w-full h-full' />
+            <img src={portfoliowomen} className='absolute inset-0 object-cover w-full h-full ' />
             {/* <div className='bubble x1'></div> */}
 
           </div>
@@ -38,11 +38,11 @@ const Home = () => {
         </div>
 
         <div className='flex-1'>
-          <div className='md:text-left text-center mt-4'>
+          <div className='mt-4 text-center md:text-left'>
             {/* <div className='bubble x2'></div> */}
-            <h1 className='md:text-4xl text-2xl md:leading-normal leading-10 text-white font-bold'>
+            <h1 className='text-2xl font-bold leading-10 text-white md:text-4xl md:leading-normal'>
               {/* <div className='bubble x9'></div> */}
-              <span className='text-cyan-600 md:text-5xl text-4xl'>
+              <span className='text-4xl text-cyan-600 md:text-5xl'>
                 Hello!
                 <br />
                 {/* <div className='bubble x3'></div> */}
@@ -50,18 +50,18 @@ const Home = () => {
               My Name is <span>Roopa Uppar</span>
 
             </h1>
-            <h4 className='md:text-2xl text-lg md:leading-normal leading-5 mt-4 font-bold text-gray-600'>Frontend Developer(React js)</h4>
+            <h4 className='mt-4 text-lg font-bold leading-5 text-gray-600 md:text-2xl md:leading-normal'>Frontend Developer(React js)</h4>
             {/* <div className='bubble x4'></div> */}
 
             {/* <button  className="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
               Contact Me
             </button> */}
-            <div className='mt-2 text-2xl flex md:justify-start justify-center gap-5'>
+            <div className='flex justify-center gap-5 mt-2 text-2xl md:justify-start'>
               {/* <div className='bubble x5'></div> */}
               {
                 social_media?.map(social_link => (
                   <>
-                    <div key={social_link} className='text-gray-600 hover:text-white cursor-pointer'>
+                    <div key={social_link} className='text-gray-600 cursor-pointer hover:text-white'>
                       {/* <div className='bubble x6'></div> */}
                       <div className='flex gap-3'>
                         <a href={social_link.live_link} target="_blank" ><ion-icon name={social_link.icons}></ion-icon></a>
